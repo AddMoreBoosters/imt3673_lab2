1d748b18e826112d44bd1a620a4b4176d86bda4a
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2018-03-16 13:12:42 +0100

    Added action bar with currently nonfunctional settings button

--- app/src/main/java/com/example/herma/lab2/MainActivity.java
+++ app/src/main/java/com/example/herma/lab2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.herma.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                //  Start the settings activity
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
